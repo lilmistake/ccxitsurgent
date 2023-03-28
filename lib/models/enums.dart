e5f27e367b465de25fe3694e38ec937480b63975
Rohit <rasinrohit@gmail.com>
2023-03-28 23:36:34 +0530

    added helper functions for colors

--- lib/models/enums.dart
+++ lib/models/enums.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 enum Status {
   available,
   busy,
@@ -5,3 +7,38 @@ enum Status {
 }
 
 enum Priority { low, medium, high }
+
+Color getPriorityColor(Priority priority) {
+  switch (priority) {
+    case Priority.low:
+      return Colors.green;
+    case Priority.medium:
+      return Colors.orange;
+    case Priority.high:
+      return Colors.red;
+    default:
+      return Colors.grey;
+  }
+}
+
+Color getStatusColor(Status status) {
+  switch (status) {
+    case Status.available:
+      return Colors.green;
+    case Status.busy:
+      return Colors.yellow;
+    case Status.dnd:
+      return Colors.red;
+  }
+}
+
+IconData getStatusIcon(Status status) {
+  switch (status) {
+    case Status.available:
+      return Icons.circle;
+    case Status.busy:
+      return Icons.watch_later_sharp;
+    case Status.dnd:
+      return Icons.do_not_disturb_on;
+  }
+}
--- lib/widgets/profile_picture.dart
+++ lib/widgets/profile_picture.dart
@@ -2,15 +2,12 @@ import 'package:ccxitsurgent/models/enums.dart';
 import 'package:flutter/material.dart';
 import 'package:ccxitsurgent/models/user_model.dart';
 
-Map<Status, Color> statusColor = {
-  Status.available: Colors.green,
-  Status.busy: Colors.yellow,
-  Status.dnd: Colors.red
-};
-
 class ProfilePicture extends StatelessWidget {
   const ProfilePicture(
-      {super.key, required this.user, this.width=75, this.includeStatus = true});
+      {super.key,
+      required this.user,
+      this.width = 75,
+      this.includeStatus = true});
   final double? width;
   final User user;
   final bool? includeStatus;
@@ -33,18 +30,18 @@ class ProfilePicture extends StatelessWidget {
         ),
         if (includeStatus!)
           Positioned(
-            bottom: width!/38,
-            right: width!/38,
+            bottom: width! / 38,
+            right: width! / 38,
             child: Container(
+              padding: const EdgeInsets.all(1),
               decoration: BoxDecoration(
-                  border: Border.all(
-                      strokeAlign: 0,
-                      color: Theme.of(context).colorScheme.background,
-                      width: 3),
-                  color: statusColor[user.status],
+                  color: Theme.of(context).colorScheme.background,
                   shape: BoxShape.circle),
-              width: width! / 4.16, // just a random factor
-              height: width! / 4.16, // just a random factor
+              child: Icon(
+                getStatusIcon(user.status),
+                size: width! / 4,
+                color: getStatusColor(user.status),
+              ),
             ),
           )
       ],
